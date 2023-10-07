efdffa9a33ef80d38fd10c562fa207b6be3ff819
cdevries@rapiddg.com <cdevries@rapiddg.com>
2023-10-07 17:13:30 -0400

    attempt to get docker compose running app+mysql

--- README.md
+++ README.md
@@ -14,8 +14,17 @@ TODO:
 
 
 Docker:
-  build: `docker build --tag htmx-demo .` (https://docs.docker.com/language/golang/build-images/)
-  run: `docker run --publish 6969:6969 htmx-demo` (https://docs.docker.com/language/golang/run-containers/)
+  app:
+    `docker run -dp 127.0.0.1:3000:3000 --mount type=volume,src=todo-db,target=/etc/todos getting-started`
+    build: `docker build --tag htmx-demo .` (https://docs.docker.com/language/golang/build-images/)
+    run: `docker run --publish 6969:6969 htmx-demo` (https://docs.docker.com/language/golang/run-containers/)
+  sql: (https://docs.docker.com/get-started/07_multi_container/)
+    create network: `docker network create htmx-demo-db`
+    start & attach sql: `docker run -d --network htmx-demo-db --network-alias mysql -v htmx-demo-db:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=secret -e MYSQL_DATABASE=demo-db mysql:8.0`
+
+  usage:
+    Don't do anything `docker run` in the terminal, this is handled by docker compose
+
 
 SQL db:
   Instance ID: 'htmx-demo-db'
--- main.go
+++ main.go
@@ -4,17 +4,52 @@ import (
 	requestHandler "htmx-demo/router"
 	"log" // self explanatory
 	"net/http"
+	"time"
 
 	// This provides HTTP client and server implementations for the app
 	"os" // Access operating system functionality
 
-	"github.com/joho/godotenv" // read from a .env file for this application
+	"database/sql" // interact with a sql database
+
+	_ "github.com/go-sql-driver/mysql" // driver for sql (necessary when using go)
+	"github.com/joho/godotenv"         // read from a .env file for this application
 )
 
 func main() {
+	log.Println("Hey! I'm over here! The logs are over here!")
 	err := godotenv.Load()
 	if err != nil {
 		log.Println("Error loading .env file")
+	}
+
+	const (
+		DB_HOST = "tcp(127.0.0.1:3306)"
+		DB_NAME = "demo-db"
+		DB_USER = "root"
+		DB_PASS = "secret"
+	)
+
+	dsn := DB_USER + ":" + DB_PASS + "@" + DB_HOST + "/" + DB_NAME + "?charset=utf8"
+
+	for {
+
+		log.Print(".")
+
+		db, err := sql.Open("mysql", dsn)
+		if err != nil {
+			log.Println("Error: " + err.Error())
+			return
+		}
+
+		err = db.Ping()
+		if err != nil {
+			log.Println("Error: " + err.Error())
+			time.Sleep(2 * time.Second)
+			continue
+		} else {
+			log.Println("Success!")
+			break
+		}
 	}
 
 	port := os.Getenv("PORT")
